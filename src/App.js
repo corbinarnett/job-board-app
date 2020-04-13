62902077f6110e3b047e62ff7c43c298c9118489
corbinarnett <corbin.c.arnett@gmail.com>
2020-04-13 14:32:35 -0700

    set initial state, import json data"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import data from "./assets/data.json";
+import JobTile from "./components/JobTile";
 
-
+// console.log(data);
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    setJobs(data);
+  }, []);
+
+  console.log(jobs);
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await fetch("https://swapi.co/api/planets/4/");
+  //     res
+  //       .json()
+  //       .then(res => setPlanets(res))
+  //       .catch(err => setErrors(err));
+  //   }
+
+  //   fetchData();
+  // });
+
   return (
     <div className="App">
       <h1>Job Listing App</h1>
+      {jobs.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        jobs.map((job) => <JobTile job={job} key={job.id} />)
+      )}
     </div>
   );
 }
